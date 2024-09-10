a55768e0fa7ea2351db1c97e84250360dd5910a8
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-09-10 10:41:07 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,5 +1,5 @@
 package lv.rvt;
-
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
@@ -8,9 +8,15 @@ public class App
     public static void main( String[] args )
     {
        System.out.println("Hello world!");
-       String name = "Nils";
        boolean isOnline = false;
        int age = 17;
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Ievadi savu vārdu: ");
+       String name = scanner.nextLine();
+       System.out.println("Ievadiet savu uzvārdu: ");
+       String lastname = scanner.nextLine();
+       System.out.println("Ievadiet savu grupu: ");
+       String group = scanner.nextLine();
+       System.out.println("Students: " + name + " " + lastname + ", Grupa: " + group);
     }
-
 }
