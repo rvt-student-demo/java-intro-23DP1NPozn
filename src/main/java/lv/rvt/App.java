b3be41d5cb0c4badd0ab9d17de0a39f588c3986c
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-09-17 10:01:53 +0000

    int uz double

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,14 +8,15 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        // saņēmām ievadi kā string vērtību
+        
+         //saņēmām ievadi kā string vērtību
         //String input = scanner.nextLine();
-        // veidojam int datu tipa mainīgo, un konvertējam to
+         //veidojam int datu tipa mainīgo, un konvertējam to
         //int inputAsInt = Integer.valueOf(input);
 
         //System.out.println(inputAsInt * 10);
-        // Izveidot programmu kura prasa ievadīt divus ciparus
-        // un izvadē gribam redzēt to ciparu reizinājumu
+         //Izveidot programmu kura prasa ievadīt divus ciparus
+         //un izvadē gribam redzēt to ciparu reizinājumu
         //System.out.println("Ievadi pirmo skaitli: ");
         //String input1 = scanner.nextLine();
         //System.out.println("Ievadi otro skaitli: ");
@@ -23,8 +24,20 @@ public class App
         //int skaitlis1 = Integer.valueOf(input1);
         //int skaitlis2 = Integer.valueOf(input2);
         //System.out.println(skaitlis1 * skaitlis2);
-        int skaitlis = Integer.valueOf(scanner.nextLine());
+        //int skaitlis = Integer.valueOf(scanner.nextLine());
         
+        //int cipars1 = Integer.valueOf(scanner.nextLine());
+        //int cipars2 = Integer.valueOf(scanner.nextLine());
+        //int cipars3 = Integer.valueOf(scanner.nextLine());
+        //int dalitajs = 3;
 
+        //double vid = (cipars1 + cipars2 + cipars3) / (double) dalitajs;
+
+        //System.out.println((cipars1 + cipars2 + cipars3)/3.0);
+
+        //int first = 1;
+        //int second = 3;
+        //double result = first / second;
+        //System.out.println(result);
     }
 }
