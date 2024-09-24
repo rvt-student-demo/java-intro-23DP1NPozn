1c5ea21e2bc7aea5f031ff156f8dd71e401759f3
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-09-24 07:49:25 +0000

    Number of Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -14,25 +14,15 @@ public class App
         //}  else {
             //System.out.println("Your number is five or less.");
         //}
-        System.out.println("Value of the gift? ");
-        int gift = Integer.valueOf(scanner.nextLine());
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        } else if (gift >= 5000 && gift < 25000) {
-            double tax=(100 + (gift-5000) * 0.08);
-            System.out.println("Tax: " + tax);
-        } else if (gift >= 25000 && gift < 55000) {
-            double tax=(1700 + (gift-25000) * 0.1);
-            System.out.println("Tax: " + tax);
-        } else if (gift >= 55000 && gift < 200000 ) {
-            double tax=(4700 + (gift-55000) * 0.12);
-            System.out.println("Tax: " + tax);
-        } else if (gift >= 200000 && gift < 1000000 ) {
-            double tax=(22100 + (gift-200000) * 0.15);
-            System.out.println("Tax: " + tax);
-        } else if (gift >= 1000000) {
-            double tax=(142100 + (gift-1000000) * 0.17);
-            System.out.println("Tax: " + tax);
+    int skait=0;
+    while (true) {
+        System.out.println("Give a number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number==0) {
+            break;
         }
-        }
-        }
+        skait+=1;
+    }
+    System.out.println("Number of numbers: "+ skait);
+    }
+    }
