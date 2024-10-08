bed6ef5ad8b0f80601654d706444388f83690da8
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-10-08 07:14:30 +0000

    Visas 4 daļas kopā vienā commit

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,7 +6,17 @@ import java.io.BufferedWriter;
 public class App 
 {
         public static void main(String[] args) {
-        printTriangle(4);
+            printStars(5);
+            printStars(3);
+            printStars(9);
+            System.out.println();
+            printSquare(4);
+            System.out.println();
+            printRectangle(17, 3);
+            System.out.println();
+            printTriangle(4);
+
+
 }
     public static void printTriangle(int size) {
         for (int x=0; x < size; x++){
@@ -16,4 +26,26 @@ public class App
             System.out.println();
         }
     }
-}
+    public static void printSquare(int number) {
+        for (int x=0; x < number; x++){
+            for (int n=0; n < number; n++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void printRectangle(int length, int width) {
+        for (int x=0; x < width; x++){
+            for (int n=0; n < length; n++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void printStars(int number) {
+        for (int n=0; n < number; n++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
