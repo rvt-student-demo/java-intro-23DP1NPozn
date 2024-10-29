6a7b083dedd4f3493421ffb014708865b45f3d55
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-10-29 06:54:49 +0000

    age of the oldest

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,19 +3,26 @@ import java.util.*;
 public class App 
 {
         public static void main(String[] args) {
-            ArrayList<Integer> numbers = new ArrayList<>();
-            Scanner scanner = new Scanner(System.in);
-            numbers.add(3);
-            numbers.add(2);
-            numbers.add(6);
-            numbers.add(-1);
-            numbers.add(5);
-            numbers.add(1);
-            System.out.println("The numbers in the range [0, 5]");
-            printNumbersInRange(numbers, 0, 5);
+            Scanner reader = new Scanner(System.in);
+            int sum = 0;
+            int x=0;
+            int max=0;
             
-            System.out.println("The numbers in the range [3, 10]");
-            printNumbersInRange(numbers, 3, 10);
+            while (true) {
+                String input = reader.nextLine();
+                if (input.equals("")) {
+                    break;
+                }
+            
+                String[] parts = input.split(",");
+                if (Integer.valueOf(parts[x])>max || max==0){
+                    max = Integer.valueOf(parts[x]);
+                }
+                x++;
+            }
+            
+            System.out.println("Age of the oldest: " + max);
+        }
             //nameList.size()
             //nameList.add()
             //nameList.get()
@@ -23,13 +30,4 @@ public class App
             //list.contains() boolean found = list.contains()
             // if (list.contains("second")) {
                 //System.out.println("Second can be found");}
-            }
-            public static void printNumbersInRange
-            (ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
-                for (int number: numbers) {
-                    if (number >= lowerLimit && number<=upperLimit){
-                        System.out.println(number);
-                    }
-                }
-            }
             }
