7051c708a2f1ca4e89edce7f07a51865310abdbe
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-10-29 12:11:01 +0000

    product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,25 +3,16 @@ import java.util.*;
 public class App 
 {
         public static void main(String[] args) {
-            Scanner reader = new Scanner(System.in);
-            int sum = 0;
-            int x=0;
-            int max=0;
-            
-            while (true) {
-                String input = reader.nextLine();
-                if (input.equals("")) {
-                    break;
-                }
-            
-                String[] parts = input.split(",");
-                if (Integer.valueOf(parts[x])>max || max==0){
-                    max = Integer.valueOf(parts[x]);
-                }
-                x++;
-            }
-            
-            System.out.println("Age of the oldest: " + max);
+            Student matt = new Student();
+            matt.play();
+            Person ada = new Person("Ada");
+            Person antti = new Person("Antti");
+            Person martin = new Person("Martin");
+    
+            ada.printPerson();
+            antti.printPerson();
+            martin.printPerson();
+
         }
             //nameList.size()
             //nameList.add()
---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+public class Product {
+    private String initialName;
+    private double initialPrice;
+    private int initialQunatity;
+    public Product
+    (String initialName, double initialPrice, int initialQuantity){
+        this.initialName = "Banana";
+        this.initialPrice = 1.1;
+        this.initialQuantity= 13;
+    }
+    public void printProduct(){
+        System.out.println();
+    }
+}
