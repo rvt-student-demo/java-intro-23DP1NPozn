fa98b61cafae025b4ca88576d12c4c6ef9232edc
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-12-16 20:04:38 +0000

    person manager 0.1

--- src/main/java/lv/rvt/PersonManager.java
+++ src/main/java/lv/rvt/PersonManager.java
@@ -1,18 +1,27 @@
 package lv.rvt;
+
 import java.io.BufferedReader;
 import java.util.ArrayList;
 
-import lv.rvt.Person;
 public class PersonManager {
-    public static ArrayList<Person> getPersonList() throws Exception{
+    public static ArrayList<Person> getPersonList() throws Exception {
         BufferedReader reader = Helper.getReader("persons.csv");
         ArrayList<Person> persons = new ArrayList<>();
         String line;
-        while ((line = reader.readLine()) !=null) {
+
+        while ((line = reader.readLine()) != null) {
             String[] parts = line.split(", ");
-            Person person = new Person(parts[0], parts[1], parts[2]);
+
+            String name = parts[0];
+            int age = Integer.parseInt(parts[1]);
+            int weight = Integer.parseInt(parts[2]);
+            int height = Integer.parseInt(parts[3]);
+
+            Person person = new Person(name, age, height, weight);
+            persons.add(person);
         }
-        
+
+        reader.close();
+        return persons;
     }
-    public int retu
 }
