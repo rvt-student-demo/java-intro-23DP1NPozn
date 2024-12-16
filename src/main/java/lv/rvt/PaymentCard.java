283cee5711afb93aab44c9c3313b40f8936f039e
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-12-16 14:23:02 +0000

    dumb payment card

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -2,8 +2,8 @@ package lv.rvt;
     public class PaymentCard {
         private double balance;
 
-        public PaymentCard(double openingBalance) {
-            this.balance = openingBalance;
+        public PaymentCard(double balance) {
+            this.balance = balance;
         }
         public void eatAffordably() {
             if (this.balance>2.60){
@@ -11,20 +11,26 @@ package lv.rvt;
             }
         }
         
+    public double balance() {
+        return this.balance;
+    }
+        
         public void eatHeartily() {
             if (this.balance>4.60){
                 this.balance-=4.60;
             }
         }
-        public void addMoney(double amount) {
-            if (amount>0){
-            this.balance+=amount;
-            }
-        }
-        public boolean takeMoney(double amount) {
-            // implement the method so that it only takes money from the card if
-            // the balance is at least the amount parameter.
-            // returns true if successful and false otherwise
-        }
 
+        public void addMoney(double increase) {
+            this.balance = this.balance + increase;
+        }
+    
+        public boolean takeMoney(double amount) {
+            // Check if there is enough balance to take the money
+            if (this.balance >= amount) {
+                this.balance -= amount; // Deduct the amount from the balance
+                return true;            // Return true indicating the withdrawal was successful
+            }
+            return false;                // Return false if there was insufficient balance
+        }
     }
