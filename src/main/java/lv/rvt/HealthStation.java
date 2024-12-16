2acb3f53b51aab0e9323a11774aee0cf0a8cc96d
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2024-12-16 16:43:54 +0000

    weighings

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,14 +1,18 @@
 package lv.rvt;
 
 public class HealthStation {
-
+    private int weighings;
 
     public int weigh(Person person) {
+        weighings++;
         return person.getWeight();
+        
     }
     public void feed(Person person) {
         int currentWeight = person.getWeight();
         person.setWeight(currentWeight + 1);
     }
-
+    public int weighings() {
+        return weighings;
+    }
 }
