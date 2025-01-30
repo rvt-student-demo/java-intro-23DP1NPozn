e60a353e5323732ca823c84a507f8366fb796e25
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2025-01-30 12:59:11 +0000

    Warehouse

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,47 +8,14 @@ public class App
     public class MainProgram {
         public static void main(String[] args) {
 
-            Engine engine = new Engine("combustion", "hz", "volkswagen", "VW GOLF 1L 86-91");
-            System.out.println(engine.getEngineType());
-            System.out.println(engine.getManufacturer());
-
-            Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
-            Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
-            System.out.println(ada);
-            System.out.println(esko);
-
-            Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-            System.out.println(ollie);
-            ollie.study();
-            System.out.println(ollie);
-
-            Teacher ada2 = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-            Teacher esko2 = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-            System.out.println(ada2);
-            System.out.println(esko2);
-
-            Student ollie2 = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-            // int i = 0;
-            // while (i < 25) {
-            // ollie2.study();
-            // i = i + 1;
-            // }
-            //System.out.println(ollie2);
-
-            ArrayList<Person> persons = new ArrayList<Person>();
-            persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-            persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
-
-            printPersons(persons);
+            ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+            juice.addToWarehouse(1000.0);
+            juice.takeFromWarehouse(11.3);
+            System.out.println(juice.getName()); // Juice
+            System.out.println(juice);           // balance = 988.7, space left 11.3
         }
-        public static void printPersons(ArrayList<Person> persons) {
-            for (Person person : persons) {
-                System.out.println(person);
-            }
-        }
-            }
-        }
+    }
+}
 
             
         //     System.out.println("Hello, choose your command");
---
+++ src/main/java/lv/rvt/ProductWarehouse.java
@@ -0,0 +1,24 @@
+package lv.rvt;
+
+public class ProductWarehouse extends Warehouse {
+    private String name;
+
+    public ProductWarehouse(String name, double capacity) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String toString() {
+        return this.name + ": " + this.getBalance() + ", " + "space left: " + this.howMuchSpaceLeft();
+    }
+
+    
+}
