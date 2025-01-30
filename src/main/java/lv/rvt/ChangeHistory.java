ac700ad1e5083d4709ba431eeb253bacf0ab9d0b
23DP1NPozn <166118460+23DP1NPozn@users.noreply.github.com>
2025-01-30 12:59:25 +0000

    History

---
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -0,0 +1,29 @@
+package lv.rvt;
+
+public class ChangeHistory {
+    private double status;
+
+    public void add(double status) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public double maxvalue() {
+
+    }
+
+    public double minValue() {
+
+    }
+
+    public double average() {
+        
+    }
+}
